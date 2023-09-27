133f4af222092de69a0e8300dcc78baef9a611ac
S R Rayhan <shafikulrahman66@gmail.com>
2023-09-27 11:56:02 +0600

    Add Close button for form, create event

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const loginSignupLink = document.querySelectorAll(".form-box .bottom-link a");
 const formPopup = document.querySelector(".form-popup");
-
 let showForm = document.getElementById("eventForm");
+const listContainer = document.getElementById("content-section");
+const fixedImage = "Images/football.png";
 
 loginSignupLink.forEach(link => {
     link.addEventListener("click",(e)=>{
@@ -12,4 +13,29 @@ loginSignupLink.forEach(link => {
 
 function openForm(){
     showForm.classList.add("open-eventForm");
+}
+
+function closeForm(){
+    showForm.classList.remove("open-eventForm");
+}
+
+
+function addEvent(){
+    let newCard = document.createElement("div");
+    newCard.className = "card";
+    const eventName = document.getElementById("eventName");
+    const eventYear = document.getElementById("eventYear");
+    const headingText = eventName.value+"-"+eventYear.value;
+
+    
+    newCard.innerHTML = `
+            <img src="${fixedImage}" alt="Task Image">
+            <h3>${headingText}</h3>
+        `;
+
+    listContainer.appendChild(newCard);
+
+    eventName.value = "";
+    eventYear.value = "";
+    closeForm();
 }
